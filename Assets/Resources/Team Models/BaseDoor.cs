39497af8d1684b9c07e232c5c8c791965379f4e6
goldgator <little.dude.jesse@gmail.com>
2024-02-13 18:53:18 -0700

    Setup basic member navigation and kick animation

---
+++ Assets/Resources/Team Models/BaseDoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseDoor : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject movementTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Resources/Team Models/Scripts/TeammateMovementController.cs
+++ Assets/Resources/Team Models/Scripts/TeammateMovementController.cs
@@ -9,7 +9,9 @@ public class TeammateMovementController : MonoBehaviour
     private Animator animator;
 
     [SerializeField]
-    private GameObject[] targets;
+    private BaseDoor[] targets;
+
+    private Coroutine movementRoutine;
 
     [SerializeField]
     private float movementInterval = 2.5f;
@@ -17,6 +19,9 @@ public class TeammateMovementController : MonoBehaviour
 
     private float movementPause = 0f;
     private float animInterval = 1f;
+
+    [SerializeField]
+    private float rotateSpeed = 90f;
 
     void Start()
     {
@@ -37,28 +42,82 @@ public class TeammateMovementController : MonoBehaviour
     {
         movementPause -= Time.deltaTime;
         animator.SetFloat("Velocity", agent.velocity.magnitude);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             animator.SetTrigger("Kick");
-            agent.isStopped = true;
             movementPause = animInterval;
-        }
-
-        if (movementPause < 0f)
-        {
-            agent.isStopped = false;
         }
     }
 
     private void HandleDestinations()
     {
-        movementTimer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveToDoor(ReturnRandomDoor());
+        }
+
+/*        movementTimer -= Time.deltaTime;
         if (movementTimer < 0)
         {
             movementTimer = movementInterval;
-            SetNewDestination();
-        }
+            MoveToDoor(ReturnRandomDoor());
+        }*/
     }
+
+    private BaseDoor ReturnRandomDoor()
+    {
+        if (targets.Length == 0) throw new UnassignedReferenceException("No Doors assigned to teammember");
+
+        return targets[Random.Range(0, targets.Length)];
+    }
+
+    private void MoveToDoor(BaseDoor baseDoor)
+    {
+        BaseDoor doorComponent = baseDoor.GetComponent<BaseDoor>();
+        GameObject targetTransform = doorComponent.movementTarget;
+        MoveToDestination(targetTransform);
+    } 
+
+    private Coroutine MoveToDestination(GameObject target)
+    {
+        if (movementRoutine != null)
+        {
+            StopCoroutine(movementRoutine);
+        }
+
+        return movementRoutine = StartCoroutine(PerformMoveToTransform(target));
+    }
+
+    private IEnumerator PerformMoveToTransform(GameObject target)
+    {
+        agent.isStopped = false;
+        Transform targetTransform = target.transform;
+        agent.SetDestination(targetTransform.position);
+
+        //Wait a frame for remaningDistance to be generated
+        yield return null;
+
+        // Wait until at destination
+        Debug.Log("Walking towards destination");
+        while (agent.remainingDistance > 0.1f) { yield return null; }
+        agent.isStopped = true;
+
+        // Rotate towards direction of movementTarget
+        Debug.Log("Arrived at destination, rotating...");
+        float angle;
+        Quaternion toRotation;
+        do
+        {
+            toRotation = targetTransform.rotation;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
+            yield return null;
+
+            angle = Quaternion.Angle(toRotation, transform.rotation);
+        } while (System.Math.Abs(angle) > 0.2);
+        Debug.Log("Rotating stopped!");
+    }
+
+
 
     private void SetNewDestination()
     {
